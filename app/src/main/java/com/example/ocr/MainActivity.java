55c3b0cb1b5c016a222dae9e2ed407c09425dad8
MathisDemonceaux <103376510+MathisDemonceaux@users.noreply.github.com>
2023-12-29 17:35:18 +0100

    Ajout déclaration des bouton dans le code

--- app/src/main/java/com/example/ocr/MainActivity.java
+++ app/src/main/java/com/example/ocr/MainActivity.java
@@ -1,12 +1,26 @@
 package com.example.ocr;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.imageview.ShapeableImageView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MaterialButton imageEntree;
+    private MaterialButton lireImage;
+    private ShapeableImageView imagePrise;
+    private EditText texteReconnue;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageEntree = findViewById(R.id.imageEntree);
+        lireImage = findViewById(R.id.lireImage);
+        imagePrise = findViewById(R.id.imagePrise);
+        texteReconnue = findViewById(R.id.texteReconnue);
     }
 }
